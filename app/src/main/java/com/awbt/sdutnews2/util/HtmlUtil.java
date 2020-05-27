acabb4a2126eb6272e3c47634df611dea94ffb75
a working binary tree <mail@jhcloud.top>
2020-05-27 12:02:42 +0800

    first finish

--- app/src/main/java/com/awbt/sdutnews2/util/HtmlUtil.java
+++ app/src/main/java/com/awbt/sdutnews2/util/HtmlUtil.java
@@ -30,12 +30,10 @@ public class HtmlUtil {
         Log.d("getlist", "getlist url:"+useurl);
         Document document = Jsoup.connect(useurl).get();
 
-        System.out.println(document.toString());
         List<NewsBean> ret = new ArrayList<NewsBean>();
         Element postList = document.select("tbody").get(2);
         Elements postItems = postList.select("tr");
         for (Element postItem : postItems) {
-            Log.d("getlist", "postitem: "+postItem.toString());
             Elements titleEle = postItem.select(".list_tit [target='_blank']");
             Elements contentEle = postItem.select(".list_content [target='_blank']");
             Elements timeEle = postItem.select(".list_time [class='lt_b']");
@@ -48,5 +46,22 @@ public class HtmlUtil {
             ret.add(data);
         }
         return ret;
+    }
+    public String getnews(String url) throws IOException {
+        Document document = Jsoup.connect(url).get();
+        Element postList1 = document.getElementsByClass("con_left clearfix").get(0);
+        Log.d("getnews", "elements:"+postList1.toString());
+        Element postList = document.getElementsByClass("wp_articlecontent").get(0);
+        Elements imgItems = postList.select("[data-layer='photo']");
+        String imgUrl;
+        for (Element imgItem: imgItems) {
+            imgUrl = baseurl + imgItem.attr("src");
+            imgItem.attr("src", imgUrl);
+            imgItem.attr("original-src", imgUrl);
+            imgItem.attr("max-width", "100%")
+                    .attr("height", "auto");
+            imgItem.attr("style", "max-width:100%;height:auto");
+        }
+        return postList1.toString();
     }
 }
